0e31d10b2af627c5a2e72ef578dc41076ef7712f
Jgawkub <jgaweda@yahoo.com>
2022-12-07 18:49:46 -0500

    edits

--- src/Components/navbar.js
+++ src/Components/navbar.js
@@ -2,14 +2,21 @@ import React from "react";
 import Navbar from 'react-bootstrap/NavBar'
 import Nav from 'react-bootstrap/Nav'
 import {Link} from "react-router-dom";
+import Container from 'react-bootstrap/Container'
 
 export default function Navigation(){
-    return(   <Navbar ariant="dark">
-        <Nav className="me-auto">
-    <Link to='/'> Home </Link>
-    <Link to='/movieform'> Movie Reviewer </Link>
-    <Link to='/feedback'> Feedback </Link>
+    return(   <Navbar id='nav' className='nav'>
+        <Container>
+        <Navbar.Brand>My Final Project</Navbar.Brand>
+        <Nav className=" n1 justify-content-end flex-grow-1 pe-3">
+            
+        
+            <li><Link className="li" to='/'>Home</Link></li>
+            <li ><Link className="li" to='/movieform'>Movie Reviewer</Link></li>
+            <li><Link className="li" to='/feedback'>Feedback </Link></li>
+         
         </Nav>
+        </Container>
             </Navbar>
 )
 }
